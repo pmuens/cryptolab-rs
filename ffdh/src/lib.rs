8ffffeda2f015d214b603d0a6bbbe37efc829572
Philipp Muens <philipp@muens.io>
2023-03-31 21:04:13 +0200

    Implement shared secret derivation

--- ffdh/src/lib.rs
+++ ffdh/src/lib.rs
@@ -1,14 +1,30 @@
+#![allow(dead_code)]
+
+#[macro_use]
+extern crate lazy_static;
+
+mod ffdh;
+mod group;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::{ffdh::FFDH, group::ID};
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn ffdh_basic() {
+        let alice_ffdh = FFDH::new(ID::Five);
+        let alice_public_key = alice_ffdh.get_public_key();
+
+        let bob_ffdh = FFDH::new(ID::Five);
+        let bob_public_key = bob_ffdh.get_public_key();
+
+        let alice_shared_secret = alice_ffdh.derive_shared_secret(&bob_public_key);
+        let bob_shared_secret = bob_ffdh.derive_shared_secret(&alice_public_key);
+
+        assert_eq!(alice_shared_secret, bob_shared_secret);
     }
 }
